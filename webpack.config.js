bd526978fae929a1e82303932196c1380464e035
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-01-12 17:52:09 +0500

    set webpack part 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Chess School",
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
