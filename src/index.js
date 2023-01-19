47737a67854cad27e7abb74a783ab920b7919cc0
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-01-19 09:45:27 +0500

    cool menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import "./styles/style.css";
+import "./styles/style.scss";
+const marker = document.querySelector("#marker");
+const item = document.querySelectorAll("ul li a");
+function Indicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
+item.forEach((link) => {
+  link.addEventListener("mousemove", (e) => {
+    Indicator(e.target);
+  });
+});
